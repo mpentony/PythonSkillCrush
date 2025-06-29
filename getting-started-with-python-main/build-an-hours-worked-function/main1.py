c535b772cd288b7dd3090427f410a1057639da76
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-06-29 14:54:37 -0500

    Update and rename main.py to main1.py

--- getting-started-with-python-main/build-an-hours-worked-function/main1.py
+++ getting-started-with-python-main/build-an-hours-worked-function/main1.py
@@ -1,8 +1,13 @@
 # Write function here
+def calculate_pay(hours_worked, pay_per_hour):
+    pay = hours_worked * pay_per_hour
+    return pay
 
 # Worked 40 hours at $20 an hour
 print(calculate_pay(40,20))
+
 # Worked 50 hours at $20 an hour
 print(calculate_pay(50,20))
+
 # Worked 40 hours at $12 an hour
 print(calculate_pay(40,12))
