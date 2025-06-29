0508ec7ae6929d1d93f0497b0daf1e03c59fceb5
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-06-29 13:52:06 -0500

    Add files via upload

--- getting-started-with-python-main/error-handling-with-try-and-except/main.py
+++ getting-started-with-python-main/error-handling-with-try-and-except/main.py
@@ -1,5 +1,13 @@
-print("Which two numbers do you want to add together?")
-
-user_input1 = input("Input your first number: ")
-user_input2 = input("Input your second number: ")
-
+user_input = input("Which birthday are you looking for? ")
+if (user_input=='John'):
+   print("Your Birthday is 12/29")
+elif (user_input== 'Monique'):
+   print("Your Birthday is 3/30")  
+elif (user_input== 'Joseph'):
+  print(" Your Birthday is 12/3")  
+elif (user_input== 'Nick'):
+  print(" Your Birthday is 10/6")
+else:
+    print("Can't find a birthday for this person.")   
+  
+  
