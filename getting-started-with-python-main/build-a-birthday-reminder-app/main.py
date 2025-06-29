cff7f10ddcb2d5311c750df2b714b3be760ef4d6
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-06-29 09:34:46 -0500

    Latest Main File

--- getting-started-with-python-main/build-a-birthday-reminder-app/main.py
+++ getting-started-with-python-main/build-a-birthday-reminder-app/main.py
@@ -1,1 +1,13 @@
 user_input = input("Which birthday are you looking for? ")
+if (user_input=='John'):
+   print("Your Birthday is 12/29")
+elif (user_input== 'Monique'):
+   print("Your Birthday is 3/30")  
+elif (user_input== 'Joseph'):
+  print(" Your Birthday is 12/3")  
+elif (user_input== 'Nick'):
+  print(" Your Birthday is 10/6")
+else:
+    print("Can't find a birthday for this person.")   
+  
+  
