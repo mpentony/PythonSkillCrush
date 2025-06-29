0a88894a7b02a71340a49aa038ba3bb9d48a96da
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-06-29 13:58:00 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Which two numbers do you want to add together?")
+
+user_input1 = input("Input your first number: ")
+user_input2 = input("Input your second number: ")
+is_good = True
+
+try:
+    int(user_input1)
+except:
+    print("Your first number needs to be an integer!")
+    is_good = False
+try:
+    int(user_input2)
+except:
+    print("Your second number needs to be an integer!")
+    is_good = False
+
+if (is_good):
+   sum = int(user_input1) + int(user_input2)    
+   print("Your sum is :", sum)
+else:
+   print ()
+   print("Your numnbers are not valid.  Please check your inputs and try again.")
+  
+  
