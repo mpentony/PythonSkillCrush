aaed1f0f528a1acea6830e17cbe0631467cc8d94
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-01 17:47:14 -0500

    Add files via upload

---
+++ getting-started-with-python-main/updating-this-list/main.py
@@ -0,0 +1,15 @@
+friendsgroup = ["John","Monique","Nick","Joseph","Chantal","Annabelle","Kreg","Emily",
+                "Myra","Rob","Jake","Natalie",
+                "Tara","Andrew","Elke","Ginger","Tamara","Susanna","Turner","Shawn"]
+
+invited = friendsgroup[:4]
+print(invited)
+
+out_for_drinks = friendsgroup[16:]
+print(out_for_drinks)
+
+hiking = friendsgroup[10:14]
+print(hiking)
+
+morning=friendsgroup[2]
+print(morning)
