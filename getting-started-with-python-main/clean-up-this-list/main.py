09c8920e21d2c67fa73f5adc569ca52cc852cb14
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-01 16:39:27 -0500

    Add files via upload

--- getting-started-with-python-main/clean-up-this-list/main.py
+++ getting-started-with-python-main/clean-up-this-list/main.py
@@ -1,1 +1,9 @@
 websites = ["www.google.com","www.youtube.com","www.dropbox.com","www.quora.com","www.reddit.com","www.instagram.com"]
+print(websites)
+websites.pop(2)
+print(websites)
+websites.insert(2,"wwww.yahoo.com")
+print(websites)
+websites.append("www.facebook.com")
+print(websites)
+print("websites =",websites)
