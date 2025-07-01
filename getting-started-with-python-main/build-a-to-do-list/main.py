9375709b0d6b3f9615327127620ad664f3ada6f9
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-01 18:35:00 -0500

    Add files via upload

--- getting-started-with-python-main/build-a-to-do-list/main.py
+++ getting-started-with-python-main/build-a-to-do-list/main.py
@@ -3,3 +3,12 @@ done = False
 
 print("Type 'done' at any time to exit")
 
+while not done:
+    task = input("Enter a task: ")
+  
+    if task == "done":
+        done = True  # Exit loop
+    else: 
+        todo_list.append(task)
+print(todo_list)   
+            
