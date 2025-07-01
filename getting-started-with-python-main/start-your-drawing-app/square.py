4f80b27cf585d1b2feeeacca4831edbed6b2077f
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-01 00:16:33 -0500

    Add files via upload

---
+++ getting-started-with-python-main/start-your-drawing-app/square.py
@@ -0,0 +1,17 @@
+import turtle
+t = turtle.Turtle()
+import turtle 
+t = turtle.Turtle()
+
+import turtle 
+t = turtle.Turtle()
+
+t.pencolor("red")
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
---
+++ getting-started-with-python-main/start-your-drawing-app/star.py
@@ -0,0 +1,14 @@
+import turtle 
+t = turtle.Turtle()
+
+t.pencolor("red")
+t.forward(110)
+t.left(216)
+t.forward(110)
+t.left(216)
+t.forward(110)
+t.left(216)
+t.forward(110)
+t.left(216)
+t.forward(110)
+t.left(216)
