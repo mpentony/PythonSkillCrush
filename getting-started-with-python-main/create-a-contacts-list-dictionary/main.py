1020bf04ee0782dcd2c1a00031dfa28e3f29d949
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-04 07:44:10 -0500

    Add files via upload

--- getting-started-with-python-main/create-a-contacts-list-dictionary/main.py
+++ getting-started-with-python-main/create-a-contacts-list-dictionary/main.py
@@ -3,3 +3,10 @@ title = ["Marketing Coordinator", "Software Developer", "Sales Representative", 
 
 company_org = {}
 
+for i in range(len(people)):
+  person = people[i]
+  person_title = title[i]
+  key = person
+  value = person_title
+  print(key + " is the " + value)
+  
