94d03c732d390f9a987e09175fff91c7ff59e521
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-04 12:31:18 -0500

    Add files via upload

--- getting-started-with-python-main/build-a-pet-shop-with-a-python-class/main.py
+++ getting-started-with-python-main/build-a-pet-shop-with-a-python-class/main.py
@@ -1,6 +1,11 @@
 class Pet:
   def __init__(self, name, species, breed, age, weight):
-
+    self.name = name
+    self.species = species
+    self.breed = breed
+    self.age = age
+    self.weight = weight
 
 my_pet = Pet("Max","Dog","Boston Terrier",3,10)
 
+print(my_pet.name)
