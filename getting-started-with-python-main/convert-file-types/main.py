d392f34a483827dffee1ad52de2aec5614923c4d
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-06 13:21:17 -0500

    Update main.py

--- getting-started-with-python-main/convert-file-types/main.py
+++ getting-started-with-python-main/convert-file-types/main.py
@@ -1,2 +1,13 @@
 import json
 show_data = []
+with open("television_shows.txt", "r") as text_file:
+    for line in text_file:
+        show_data.append(line)
+#print all of the data in the console      
+#print(show_data)
+
+with open("best_shows.json", "w") as write_file:
+  json.dump(show_data,write_file)
+  print(write_file)
+  print("done")
+  
