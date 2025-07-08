8e162c94b099962164e15627da411aa09eb7bffd
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-07 23:58:20 -0500

    Add files via upload

--- getting-started-with-python-main/level-up-your-language-translator/main.py
+++ getting-started-with-python-main/level-up-your-language-translator/main.py
@@ -1,21 +1,26 @@
-translations = {
-  "hello":"hola",
-  "thank you":"gracias",
-  "sorry":"lo siento"
-}
+import csv
+
+translations = {}
+
+with open('translations.csv', mode='r') as words:
+    reader = csv.DictReader(words, delimiter=",")
+    for line in reader:
+        english = line['English'].lower()
+        spanish = line['Spanish'].lower()
+        french = line['French'].lower()
+        translations[english] = [spanish, french]
+      
 
 done = False
 
-print('Type "done" at any time to exit.')
+print('Type "done" at any time to exit')
 
 while not done:
-  word = input("Type an English word to translate: ")
-  word = word.lower()
-
-  if word == "done":
-    done = True
-  elif word in translations:
-    print(translations[word])
-  else:
-    print("Translation is not known")
-
+    word = input("What English word would you like to translate?")
+    word = word.lower()
+    if word == "done":
+        done=True
+    elif word in translations:
+        print(translations[word])
+    else:
+        print("Word is not in translation dictionary")
