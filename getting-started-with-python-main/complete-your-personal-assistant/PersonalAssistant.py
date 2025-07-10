8e843a467b0ee7ddabafb0b638d14a5bdbed5917
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-09 23:30:02 -0500

    Add files via upload

--- getting-started-with-python-main/complete-your-personal-assistant/PersonalAssistant.py
+++ getting-started-with-python-main/complete-your-personal-assistant/PersonalAssistant.py
@@ -1,14 +1,8 @@
 class PersonalAssistant:
-    def __init__(self, todos, birthdays):
-
+    def __init__(self, todos, birthdays, contacts):
         self.todos = todos
         self.birthdays = birthdays
-
-    def get_contact(self, name):
-      if name in self.contacts:
-        return self.contacts[name]
-      else:
-        return "No contact with that name!"
+        self.contacts = contacts
 
     def add_todo(self, new_item):
       self.todos.append(new_item)
@@ -29,24 +23,47 @@ class PersonalAssistant:
     
     def get_birthdays(self):
       return self.birthdays
+    
+    def get_contacts(self):
+        return self.contacts
 
     def get_birthday(self, name):
       if name in self.birthdays:
-        return f"{name}'s birthday is on {self.birthdays[name]}."
+          return f"{name}'s birthday is on {self.birthdays[name]}."
       else:
-        return "Can't find birthday for this person."
-
+          return ("Birthday can not be found")
+        
+           
     def add_birthday(self, name, date):
-      if name in self.birthdays:
-          return f"You already have a birthday for {name}"
-      else:
-          self.birthdays[name] = date
+       if name in self.birthdays:
+          return f"{name}'s birthday is already on file."
+       else:
+          self.birthdays[name]=date
           return f"{name}'s birthday has been added."
-
+        
     def remove_birthday(self, name):
-      if name in self.birthdays:
-          self.birthdays.pop(name)
-          return f"{name}'s birthday has been removed."
-      else:
-          return "Sorry, there is no recorded birthday for that person."
-
+       if name in self.birthdays:
+           self.birthdays.pop(name)
+           return f"{name}'s birthday has been removed from the file"
+       else:
+           return "Sorry, there is no recorded birthday for that person."
+        
+    def get_contact(self, name):
+       if name in self.contacts:
+          return f"{name} is a {self.contacts[name]}."
+       else:
+          return ("Sorry there is no contact by that name")
+        
+    def add_contact(self, name, position):
+       if name in self.contacts:
+           return f"You already have a contact by that name."
+       else:
+           self.contacts[name] = position
+           return f"You have added {name}"
+        
+    def remove_contact(self, name):
+       if name in self.contacts:
+           self.contacts.pop(name)
+           return f"{name} has been removed from the file"
+       else:
+           return "Sorry, there is no recorded contact."    
--- getting-started-with-python-main/complete-your-personal-assistant/main.py
+++ getting-started-with-python-main/complete-your-personal-assistant/main.py
@@ -3,11 +3,12 @@ import json
 from PersonalAssistant import PersonalAssistant
 
 #ADD CODE: open JSON file and pass data to PersonalAssistant class
-with open("todo.json", "r") as todos, open("birthdays.json", "r") as birthdays:
-    todo_list = json.load(todos)
-    birthday_list = json.load(birthdays)
+with open("todo.json", "r") as todos, open("birthdays.json", "r") as birthdays, open("contacts.json", "r") as contacts:
+  todo_list = json.load(todos)
+  birthday_list = json.load(birthdays)
+  contact_list = json.load(contacts)
 
-    assistant = PersonalAssistant(todo_list, birthday_list)
+  assistant = PersonalAssistant(todo_list,birthday_list,contact_list)
 
 done = False
 
@@ -20,10 +21,14 @@ How can I help you?
     1: Add a to-do
     2: Remove a to-do
     3: Get to-do list
-    **** Birthdays ***** 
+    **** Birthdays ****
     4: Get Birthday
     5: Add Birthday
     6: Remove Birthday
+    **** Contacts *****
+    7: get a Single Contact
+    8: Add a Contact
+    9: Delete a Contact
 
     Select a number or type 'Exit' to quit: 
     
@@ -42,35 +47,48 @@ How can I help you?
     elif user_command == "3":
         print("\nYour to-do list")
         print(f"\n {assistant.get_todos()}")
-      # Get Birthday
     elif user_command == "4":
         print("Your birthday contacts: \n")
         for name in assistant.get_birthdays():
             print(name)
         user_input = input("\nEnter a name: ")
         print(f"\n{assistant.get_birthday(user_input)}")
-    # Add Birthday
     elif user_command == "5":
         print("Add a birthday: \n")
-        name = input("Name of the person: ")
+        name=input("Name of the person: ")
         birthday = input("Their birthday (ex: 08/18/2000): ")
         print(f"\n{assistant.add_birthday(name, birthday)}")
-    # Remove Birthday
-    elif user_command == "6":
+    elif user_command == "6":  
         print("Your birthday contacts: \n")
         for name in assistant.get_birthdays():
             print(name)
         user_input = input("\nWhich birthday do you want to remove? ")
-        print(f"\n{assistant.remove_birthday(user_input)}")
+        print(f"\n{assistant.remove_birthday(user_input)}")    
+    elif user_command == "7":
+        print("Your contact names are: \n")
+        for name in assistant.get_contacts():
+            print(name)
+        user_input = input("\nEnter a name: ")
+        print(f"\n{assistant.get_contact(user_input)}")
+    elif user_command == "8":
+        name=input("Add a contact name: ")
+        title=input("Contact's job title: ")
+        print(f"\n{assistant.add_contact(name,title)}")
+    elif user_command == "9":
+        print("Your contacts are: \n")
+        for name in assistant.get_contacts():
+            print(name)
+        user_input = input("\nWhich contact do you want to remove? ")
+        print(f"\n{assistant.remove_contact(user_input)}")   
     elif user_command == "exit" or user_command == "Exit" or user_command == "EXIT":
         done = True
         print("\nGoodbye, see you soon!")
     else:
         print("\nNot a valid command.")
 
-
-with open("todo.json", "w") as write_todos, open(
-    "birthdays.json", "w"
-) as write_birthdays:
+# ADD CODE: write data to JSON file
+with open("todo.json", "w") as write_todos, open("birthdays.json", "w") as write_birthdays, open("contacts.json", "w") as write_contacts:
     json.dump(assistant.get_todos(), write_todos)
     json.dump(assistant.get_birthdays(), write_birthdays)
+    json.dump(assistant.get_contacts(), write_contacts)
+    
